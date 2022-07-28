5e301dffc038b4af67fe260318c819bc5a44f91a
rogeraguiar0 <aguiarroger02@gmail.com>
2022-07-28 14:45:04 -0300

    Entrega sexta feira

--- src/js/dataProdutos.js
+++ src/js/dataProdutos.js
@@ -1,24 +1,24 @@
 const produtos = [
     {
-        nome: 'Banana',
-        preco: 2,
-        secao: 'Hortifruti',
-        categoria: 'fruta',
-        img: "./src/img/banana.png"
-    },
-    {
         nome: 'Morango',
         preco: 2,
         secao: 'Hortifruti',
-        categoria: 'fruta',
+        categoria: 'Frutas',
         img: './src/img/morango.png'
     },
     {
-        nome: 'Maçã',
+        nome: 'Banana',
         preco: 2,
         secao: 'Hortifruti',
-        categoria: 'fruta',
-        img: './src/img/maça.png'
+        categoria: 'Frutas',
+        img: "./src/img/banana.png"
+    },
+    {
+        nome: 'Yogurt de morango',
+        preco: 3,
+        secao: 'Laticínios',
+        categoria: 'Leites',
+        img: './src/img/yogurt-morango.png'
     },
     {
         nome: 'Pão',
@@ -30,8 +30,29 @@ const produtos = [
     {
         nome: 'Leite',
         preco: 5,
-        secao: 'Laticínio',
-        categoria: 'Leite',
+        secao: 'Laticínios',
+        categoria: 'Leites',
         img: './src/img/leite.png'
+    },
+    {
+        nome: 'Bolo',
+        preco: 10,
+        secao: 'Panificadora',
+        categoria: 'Bolo',
+        img: './src/img/bolo.png'
+    },
+    {
+        nome: 'Torta de maçã',
+        preco: 4,
+        secao: 'Panificadora',
+        categoria: 'Tortas',
+        img: './src/img/torta-maça.png'
+    },
+    {
+        nome: 'Maçã',
+        preco: 5,
+        secao: 'Hortifruti',
+        categoria: 'Frutas',
+        img: './src/img/maça.png'
     }
 ]
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,87 @@
-// seu código aqui
+const container = document.querySelector("ul")
+const input     = document.querySelector("input")
+
+const total = document.querySelector("#total")
+let contador = 0;
+
+const buttonTodos        = document.querySelector("#todos");
+const buttonHorti        = document.querySelector("#hortiFruti");
+const buttonPanificadora = document.querySelector("#panificadora");
+const buttonLaticinios   = document.querySelector("#laticinios");
+
+// Criação inicial de cards
+
+function criarCards (element) {    
+    const tagLi   = document.createElement("li");
+    const tagImg  = document.createElement("img");
+    const tagH3   = document.createElement("h3");
+    const tagP    = document.createElement("p");
+    const tagSpan = document.createElement("span");
+
+    tagImg.src        = element.img;
+    tagH3.innerText   = element.nome;
+    tagSpan.innerText = `R$${element.preco}.00`;
+    tagP.innerText    = element.secao;
+
+    contador += element.preco
+    total.innerText = `R$${contador}.00`;
+
+    tagLi.appendChild(tagImg)
+    tagLi.appendChild(tagH3)
+    tagLi.appendChild(tagSpan)
+    tagLi.appendChild(tagP)
+
+    return container.appendChild(tagLi)
+}
+produtos.forEach((element) => criarCards(element))
+
+// Evento dos botões
+
+buttonTodos.addEventListener("click", () => {
+    container.innerHTML = ""
+    total.innerHTML = `R$${contador}.00`
+    contador = 0;
+    produtos.forEach((element) => criarCards(element))
+});
+buttonHorti.addEventListener("click", () => {
+    container.innerHTML = ""
+    total.innerHTML = `R$${contador}.00`
+    contador = 0;
+
+    produtos.map((element) => {
+        element.secao == "Hortifruti" ? criarCards(element) : false
+    })
+});
+buttonPanificadora.addEventListener("click", () => {
+    container.innerHTML = ""
+    total.innerHTML = `R$${contador}.00`
+    contador = 0;
+
+    produtos.map((element) => {
+        element.secao == "Panificadora" ? criarCards(element) : false
+    })
+});
+buttonLaticinios.addEventListener("click", () => {
+    container.innerHTML = ""
+    total.innerHTML = `R$${contador}.00`
+    contador = 0;
+
+    produtos.map((element) => {
+        element.secao == "Laticínios" ? criarCards(element) : false
+    })
+});
+
+// Input de pesquisa
+
+input.addEventListener("keyup", (event) => {
+    container.innerHTML = ""
+    contador = 0;
+
+    produtos.map((element) => {
+        let pesquisa = input.value.toLowerCase().trim();
+        let nome     = element.nome.toLowerCase().trim();
+        let secao    = element.secao.toLowerCase().trim();
+        
+        nome.includes(pesquisa) || secao.includes(pesquisa) ? criarCards(element) : false
+    })
+});
